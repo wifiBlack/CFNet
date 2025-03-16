559bf51bb19103e432d2c08c888d200074fcde6b
Fan Wu <wifibk@whu.edu.cn>
2025-03-16 15:10:56 +0800

    Update playground.py

--- playground.py
+++ playground.py
@@ -5,7 +5,9 @@ from loss.loss import *
 from torch.utils.tensorboard import SummaryWriter
 if __name__ == '__main__':
     # start = time.time()
-    device = torch.device("cuda:3" if torch.cuda.is_available() else "cpu")
+    os.environ["CUDA_VISIBLE_DEVICES"] = "2"
+    os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
     x1 = torch.randn(1, 3, 256, 256).to(device)
     x2 = torch.randn(1, 3, 256, 256).to(device)
@@ -13,13 +15,14 @@ if __name__ == '__main__':
     # net = nn.DataParallel(CFNet(3, 3),device_ids=[0,1,2,3])
     net = CFNet(3, 3)
     net = net.to(device)
-    y_change, Y1_change, Y2_change,Y1_unchange,Y2_unchange = net(target,x1, x2,device)
+    with autocast(device.type+":"+str(device.index)):
+        y_change, Y1_change, Y2_change,Y1_unchange,Y2_unchange = net(x1, x2,device=device)
     loss = Loss()
     res = loss(y_change, Y1_change, Y2_change, Y1_unchange, Y2_unchange, target)
     from fvcore.nn import FlopCountAnalysis,parameter_count_table
     flops = FlopCountAnalysis(net, (x1, x2))
     total = sum([param.nelement() for param in net.parameters()])
     print("Params_Num: %.2fM" % (total/1e6))
-    print(flops.total()/1e9)
-    print(parameter_count_table(net))
+    # print(flops.total()/1e9)
+    # print(parameter_count_table(net))
     # end = time.time()
